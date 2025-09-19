d337b8ccfa9f638ca1c9f10374be3ea8b4e18d56
Daniel Li <inflmts@gmail.com>
2025-09-18 22:43:13 -0400

    Bootstrap frontend

---
+++ Frontend/app/layout.tsx
@@ -0,0 +1,7 @@
+export default function RootLayout(props: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{props.children}</body>
+    </html>
+  );
+}
---
+++ Frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Stylr</h1>
+      <p>Find your next fashion statement.</p>
+    </>
+  );
+}
