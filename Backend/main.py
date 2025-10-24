a7a14602347922b1009540f390e5c069ec2101cd
Daniel Li <inflmts@gmail.com>
2025-10-24 11:50:16 -0400

    Remove a few imports
    Due to the unconventional way we're hosting the backend,
    a new Python process is spawned for every request.
    This is fundamentally inefficient, but we can try to cut down startup
    times a little by pruning modules.

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from http import HTTPStatus as HTTP
-from urllib.parse import parse_qs
 import hmac
 import json
 import os
@@ -10,20 +8,32 @@ import time
 
 SECRET_KEY = os.getenv("STYLR_SECRET_KEY", "gurleen_dhillon").encode()
 
+http_status_map = {
+    200: "OK",
+    400: "Bad Request",
+    401: "Unauthorized",
+    404: "Not Found",
+    405: "Method Not Allowed",
+    500: "Internal Server Error",
+}
+
 class Request:
     def __init__(self, method, path, query):
         self.method = method
         self.path = path
-        self.query = query
-        self.params = parse_qs(query)
+        self.params = {}
+        for item in query.split("&"):
+            key, _, value = item.partition("=")
+            if key:
+                self.params[key] = value
 
     def auth(self):
         if "access_token" not in self.params:
-            raise Response(HTTP.UNAUTHORIZED, "No access token supplied")
-        token = self.params["access_token"][0]
+            raise Response(401, "No access token supplied")
+        token = self.params["access_token"]
         uid = swt_decode(token)
         if uid is None:
-            raise Response(HTTP.UNAUTHORIZED, "Invalid access token")
+            raise Response(401, "Invalid access token")
         return uid
 
     def json(self):
@@ -31,13 +41,12 @@ class Request:
         try:
             return json.loads(data)
         except:
-            raise Response(HTTP.BAD_REQUEST, "Could not parse request body")
+            raise Response(400, "Could not parse request body")
 
 class Response(Exception):
     def __init__(self, status, body=None):
         self.status = status
-        self.body = ({"message": status.phrase} if body is None else
-                     {"message": body} if isinstance(body, str) else body)
+        self.body = body
 
 def swt_encode(uid, expire=86400):
     expire += time.time_ns() // 1000000000
@@ -71,12 +80,12 @@ api.endpoints = {}
 
 @api("/ok")
 def GET(req):
-    return Response(HTTP.OK, "Welcome to Stylr!")
+    return Response(200, "Welcome to Stylr!")
 
 @api("/uid")
 def GET(req):
     uid = req.auth()
-    return Response(HTTP.OK, {"uid": uid})
+    return Response(200, {"uid": uid})
 
 @api("/login")
 def POST(req):
@@ -87,16 +96,16 @@ def POST(req):
         password = data["password"]
         assert isinstance(password, str)
     except:
-        return Response(HTTP.BAD_REQUEST)
+        return Response(400)
 
     from database import Connection
     conn = Connection()
     uid = conn.verify_user(username, password)
     conn.close()
     if uid is None:
-        return Response(HTTP.UNAUTHORIZED, "Invalid credentials. Please try again.")
+        return Response(401, "Invalid credentials. Please try again.")
     token = swt_encode(uid)
-    return Response(HTTP.OK, {"access_token": token})
+    return Response(200, {"access_token": token})
 
 @api("/register")
 def POST(req):
@@ -107,15 +116,15 @@ def POST(req):
         password = data["password"]
         assert isinstance(password, str)
     except:
-        return Response(HTTP.BAD_REQUEST)
+        return Response(400)
 
     from database import Connection
     conn = Connection()
     uid = conn.create_user(username, password)
     if uid is None:
-        return Response(HTTP.UNAUTHORIZED, "This username is already taken.")
+        return Response(401, "This username is already taken.")
     token = swt_encode(uid)
-    return Response(HTTP.OK, {"access_token": token})
+    return Response(200, {"access_token": token})
 
 #---------------------------------------
 
@@ -138,10 +147,10 @@ def main():
             path = path[4:]
         methods = api.endpoints.get(path)
         if methods is None:
-            raise Response(HTTP.NOT_FOUND)
+            raise Response(404)
         handler = methods.get(method)
         if handler is None:
-            raise Response(HTTP.METHOD_NOT_ALLOWED)
+            raise Response(405)
         response = handler(Request(method, path, query))
     except Response as e:
         response = e
@@ -149,15 +158,18 @@ def main():
         response = Response(HTTP.NOT_IMPLEMENTED)
 
     prefix = "HTTP/1.1" if direct else "status:"
+    phrase = http_status_map[response.status]
+    body = response.body
+    body = ({"message": phrase} if body is None else
+            {"message": body} if isinstance(body, str) else body)
+
     sys.stdout.write(
-        f"{prefix} {response.status} {response.status.phrase}\r\n"
+        f"{prefix} {response.status} {phrase}\r\n"
         "access-control-allow-origin: *\r\n"
         "content-type: application/json\r\n"
         "\r\n"
+        f"{json.dumps(body, indent=2)}\n"
     )
-
-    if response.body is not None:
-        sys.stdout.write(json.dumps(response.body, indent=2) + "\n")
 
 if __name__ == "__main__":
     main()
