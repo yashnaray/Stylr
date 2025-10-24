ada577ce02611d1a515409469f7f2f95cb960efb
Daniel Li <inflmts@gmail.com>
2025-10-24 10:59:15 -0400

    Add automatic postgres startup to Vite config
    Now the entire development environment can be initialized simply by
    changing to the `Frontend` directory and running `pnpm dev`.
    A custom plugin will automatically launch the PostgreSQL server
    and the backend Python processes as needed.

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,30 +1,60 @@
-import { execFile } from "node:child_process";
+import { execFile, execFileSync, spawn } from "node:child_process";
+import { mkdirSync } from "node:fs";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+
+const databaseDirectory = "../Backend/postgres";
+
+function initDatabase() {
+  try {
+    mkdirSync(databaseDirectory);
+  } catch (e) {
+    if (e.code === "EEXIST") {
+      return; // database directory exists, we are done
+    }
+    throw e;
+  }
+  const args = ["-A", "trust", "-U", "stylr", "-D", databaseDirectory];
+  execFileSync("initdb", args, { stdio: "inherit" });
+}
+
+/** @type {import("vite").ServerHook} */
+function configureServer(server) {
+  initDatabase();
+  const postgres = spawn("postgres", ["-p", "3101", "-D", databaseDirectory], {
+    stdio: ["ignore", "inherit", "inherit"]
+  });
+  server.httpServer.on("close", () => {
+    postgres.kill("SIGINT");
+  });
+  process.on("SIGINT", () => {
+    server.close();
+  });
+  server.middlewares.use("/api", (req, res) => {
+    const options = {
+      cwd: "../Backend",
+      encoding: "buffer"
+    };
+    const args = ["main.py", req.method, req.url];
+    const proc = execFile("python3", args, options, (error, stdout, stderr) => {
+      if (error) {
+        res.socket.write("HTTP/1.1 500 Internal Server Error\r\ncontent-type: text/plain\r\n\r\n");
+        res.socket.end(stderr);
+        process.stderr.write(`${req.method} ${req.url}\n`);
+        process.stderr.write(stderr);
+      } else {
+        res.socket.end(stdout);
+        process.stderr.write(stderr);
+      }
+    });
+    req.pipe(proc.stdin);
+  });
+}
 
 /** @type {import("vite").Plugin */
 const backend = {
   name: "backend",
-  configureServer(server) {
-    server.middlewares.use("/api", (req, res) => {
-      const options = {
-        cwd: "../Backend",
-        encoding: "buffer"
-      };
-      const proc = execFile("python3", ["main.py", req.method, req.url], options, (error, stdout, stderr) => {
-        if (error) {
-          res.socket.write("HTTP/1.1 500 Internal Server Error\r\ncontent-type: text/plain\r\n\r\n");
-          res.socket.end(stderr);
-          process.stderr.write(`${req.method} ${req.url}\n`);
-          process.stderr.write(stderr);
-        } else {
-          res.socket.end(stdout);
-          process.stderr.write(stderr);
-        }
-      });
-      req.pipe(proc.stdin);
-    });
-  }
+  configureServer
 };
 
 /** @type {import("vite").UserConfig */
@@ -34,5 +64,6 @@ export default {
     modulePreload: {
       polyfill: false
     }
-  }
+  },
+  clearScreen: false
 };
